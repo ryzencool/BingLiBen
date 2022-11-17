c860861dbfaa44276062454c19ce181101afb54c
zmyjust <zmyjust@gmail.com>
2022-11-17 21:10:29 +0800

    init

--- App.js
+++ App.js
@@ -1,10 +1,30 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, Image, Platform } from 'react-native';
+import React, {useState, useEffect} from 'react'
+import * as ImagePicker from 'expo-image-picker';
 
 export default function App() {
+  const [image, setImage] =  useState(null)
+  const pickImage = async () => {
+    // No permissions request is necessary for launching the image library
+    let result =  await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.All,
+      allowsEditing: true,
+      aspect: [4, 9],
+      quality: 1,
+    });
+
+    console.log(result);
+
+    if (!result.canceled) {
+      setImage(result.assets[0].uri);
+    }
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+          <Button title="Pick an image from camera roll" onPress={pickImage} />
+          {image && <Image source={{ uri: image }} style={{ width: 200, height: 200 }} />}
       <StatusBar style="auto" />
     </View>
   );
